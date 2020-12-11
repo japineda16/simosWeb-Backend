a7d3a2fc5893c61a615e952e489335e1dccff7a0
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2020-12-10 21:47:42 -0400

    JSON field added in payments

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -10,6 +10,13 @@ class Payment extends Model
     use HasFactory;
     protected $fillable = [
         'cash', 'multipago', 'wire_transfer', 'zelle', 'amount_zelle',
-        'amount_transfer', 'amount_multipago', 'total_orders', 'total'
+        'amount_transfer', 'amount_multipago', 'total_orders', 'total',
+        'method'
     ];
+    public $json;
+
+    protected $casts = [
+        'method' => 'array',
+    ];
+
 }
---
+++ database/migrations/2020_12_09_145553_drop_payments_fields.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropPaymentsFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('cash');
+            $table->dropColumn('multipago');
+            $table->dropColumn('wire_transfer');
+            $table->dropColumn('zelle');
+            $table->dropColumn('amount_zelle');
+            $table->dropColumn('amount_transfer');
+            $table->dropColumn('amount_multipago');
+            $table->json('method');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
