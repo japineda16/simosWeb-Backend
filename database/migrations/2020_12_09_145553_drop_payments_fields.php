8ba25c81f6fb81277892102bbd6a49a8c5cb40ee
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2021-05-16 12:53:19 -0400

    not null

--- database/migrations/2020_12_09_145553_drop_payments_fields.php
+++ database/migrations/2020_12_09_145553_drop_payments_fields.php
@@ -21,7 +21,7 @@ class DropPaymentsFields extends Migration
             $table->dropColumn('amount_zelle');
             $table->dropColumn('amount_transfer');
             $table->dropColumn('amount_multipago');
-            $table->json('method')->nullable();
+            $table->json('method');
         });
     }
 
